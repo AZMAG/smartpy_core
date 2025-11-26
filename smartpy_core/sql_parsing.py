2e681719ed02a3f4c4cca52580e9705ab1d0ccdf
Scott Bridwell <bridwell@gmail.com>
2025-11-26 14:27:49 -0700

    sql parsing - add parsing from and where

--- smartpy_core/sql_parsing.py
+++ smartpy_core/sql_parsing.py
@@ -2,14 +2,16 @@
 Use this module to parse query components (columms, tables used)
 from a sql query.
 
-In progress.
+In progress. Still testing. May be some edge cases that cause problems.
 
 """
 
 import re
-from dataclasses import dataclass
+import pandas as pd
+from smartpy_core.wrangling import cache
 
 
+@cache
 def get_query_sections(q: str) -> dict[str, str]:
     """
     Given a sql query, returns a dictionary of the
@@ -26,10 +28,12 @@ def get_query_sections(q: str) -> dict[str, str]:
     Returns:
     --------
     dict[str, str]: keys are statements/clauses, values are the 
-        extract str sections
+        extracted str sections. If the section does not exist 
+        it will have an entry of None.
     
     """
-    q = ' ' + q
+    # normalize
+    q = ' ' + re.sub(r'\s+', ' ', q.strip())
 
     # list of statements to check
     statements = [
@@ -56,16 +60,160 @@ def get_query_sections(q: str) -> dict[str, str]:
     m = re.search('(?<= {} )(.*)'.format(s_curr), q, flags=re.IGNORECASE)
     if m:
         res[s_curr] = m.group(1)
-    else:
-        print(':(')
 
     return res
 
 
-def get_sel_parts(q: str) -> list[str]:
+@cache
+def get_from_parts(q: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Parses the components of the `from` section of the query.
+
+    Parameters:
+    ----------
+    q: str
+        The query to parse.
+    
+    Returns:
+    --------
+    tables: pandas.DataFrame
+        Tables used in `from`, along with the source table
+        and a flag indicating if the table is using an alias.
+    columns: pandas.DataFrame
+        Columns used in join operations. The table and 
+        the source table. 
+
+    """
+    f = get_query_sections(q)['from']
+    if f is None:
+        return None, None
+
+    # break apart by join
+    join_pattern = r'join|inner|left|right|full|outer'
+    j_split = re.split(join_pattern, f, flags=re.IGNORECASE)
+    j_split = [j.strip() for j in j_split if j.strip() != '']
+
+    # break out tables and join columns
+    tabs = {}
+    cols = {}
+
+    for j in j_split:
+        j2 = re.sub(r' as ', ' ', j, flags=re.IGNORECASE)
+        on_sp = re.split(r' on ', j2, flags=re.IGNORECASE)
+        
+        # table parts -- name and potentially alias
+        tab = on_sp[0]
+        alias = None
+        tab_key = tab
+        
+        tab_sp = tab.split(' ')
+        if len(tab_sp) == 2:
+            tab = tab_sp[0]
+            alias = tab_sp[1]
+            tab_key = alias        
+        
+        tabs[tab_key] = {'src': tab, 'is_alias': int(tab != alias)}
+
+        # column parts - column name and potential table name/alias
+        # ...this may need more consideration for more complex join 
+        # ...operators
+        if len(on_sp) > 1:
+            operator_pattern = r"[+\-*/=]" 
+            op_parts = re.split(operator_pattern, on_sp[1])
+            for p in op_parts:
+                p = p.strip()
+
+                # by default assume no table, just the column
+                col = p
+                col_tab = None
+                col_src_tab = None
+
+                # separate table from column if present
+                p_pos = p.rfind('.')
+                if p_pos != -1:
+                    col = p[p_pos + 1:]
+                    col_tab = p[0:p_pos]
+                    col_src_tab = tabs[col_tab]['src']
+
+                cols[(col, col_tab)] = col_src_tab
+    
+    # return as data frames
+    tabs_df = pd.DataFrame.from_dict(tabs, orient='index')
+    tabs_df.index.name = 'table'
+    
+    cols_df = pd.Series(cols)
+    cols_df.index.names = ['column', 'table']
+    cols_df.name = 'src table'
+    cols_df = cols_df.reset_index()
+    
+    return tabs_df, cols_df    
+
+
+def get_cols_in_expr(expr: str) -> list[str]:
+    """
+    Returns a list of column names in a sql 
+    where expression or a single line of a select statement.
+
+    Parameters:
+    -----------
+    expr: str
+        The expression to evaluate.
+
+    Returns:
+    --------
+    list[str]: 
+        List of unique column names in the expression.
+        If a table name prefixes the column this will be preserved.
+
+    Sample usage:
+    -------------
+    cols = get_cols_in_expr("city in ('Chandler', 'Tempe', 'Phoenix')")
+    >>> ['city']
+
+    cols = get_cols_in_expr("city is not null or county = 'Maricopa'"))
+    >>> ['county', 'city']
+
+    cols = get_cols_in_expr(  
+        '''
+        IIF(isnull(B01001003, 0) + isnull(B01001004, 0) + isnull(B01001005, 0) + isnull(B01001006, 0) + isnull(B01001027, 0) + 
+        isnull(B01001028, 0) + isnull(B01001029, 0) + isnull(B01001030, 0) <> 0, (cast(isnull(B10001001, 0) as float)) 
+        / (isnull(B01001003, 0) + isnull(B01001004, 0) + isnull(B01001005, 0) + isnull(B01001006, 0) + isnull(B01001027, 0) + 
+        isnull(B01001028, 0) + isnull(B01001029, 0) + isnull(B01001030, 0)) * 100, 0)
+        '''
+    )
+    >>> ['B01001005', 'B01001003', 'B01001006','B01001029','B01001030','B01001027','B01001004','B01001028','B10001001']
+
+    """
+    # reserverd words
+    reserved = [ 'and', 'or', 'is', 'not', 'null', 'in', 'like', 'between']
+
+    # remove dtypes casting 
+    cast_pattern = rf"{re.escape(' as ')}(.*?){re.escape(')')}"
+    expr = re.sub(cast_pattern, '', expr, flags=re.IGNORECASE)
+
+    # remove data inside quote, e.g. 'uno'
+    expr = re.sub(rf"\S+{re.escape('(')}", '', expr)
+
+    # remove function names e.g. isnull(my_col, 10)
+    expr = re.sub(r'\'(.*?)\'', '', expr)
+
+    # extract words/parts -- retain periods for cases of table.col 
+    period_delim = '____period____'
+    expr = expr.replace('.', period_delim)
+    parts = re.findall(r'[a-zA-Z]\w*', expr)
+    parts = [p.replace(period_delim, '.') for p in parts]
+    
+    # remove reserved keywords
+    parts = list(set([p for p in parts if p.lower() not in reserved]))
+
+    return parts
+
+
+@cache
+def get_sel_parts(q: str) -> pd.DataFrame:
     """
     Parse the `select` section of a sql query 
-    into a list w/ an entry for each column.
+    into a dataframe/ 
 
     Parameters:
     -----------
@@ -74,7 +222,9 @@ def get_sel_parts(q: str) -> list[str]:
 
     Returns:
     ---------
-        
+    pandas.DataFrame with columns"
+    - column: the output column name
+    - expr: the sql expression
 
     """
     # get the select section
@@ -83,6 +233,10 @@ def get_sel_parts(q: str) -> list[str]:
     if sel is None:
         return {}
     
+    # also get the table parts
+    # not using this right now, see table comments below
+    #f_tabs, f_cols = get_from_parts(q)
+
     # parse so we have an item for each column
     LP_DELIM = '____L_PARENTH____'
     RP_DELIM = '____R_PARENTH____'
@@ -130,6 +284,7 @@ def get_sel_parts(q: str) -> list[str]:
         # ...by default assume column name is the same as the expression
         col = s
         expr = s
+        src_tab = None
 
         if s.endswith(']'):
             # bracketed cols
@@ -147,18 +302,26 @@ def get_sel_parts(q: str) -> list[str]:
                 expr = s[0:col_start_pos].strip()
 
         # pull out table name if needed
+        # **NOT DOING THIS RIGHT NOW **
+        # **COULD BE MULTIPLE TABLES SO THIS DOESNT TOTALLY MAKE SENSE
         # ...if the column has it specified
         # ...note right now this is just the alias
-        tab = None
-        if '.' in col:
-            col_split = col.split('.')
-            assert len(col_split) == 2
-            tab = col_split[0]
-            col = col_split[1]
-        
+        #tab = None
+        #p_pos = col.rfind('.')
+        #if p_pos != -1:
+        #    tab = col[0:p_pos]
+        #    col = col[p_pos + 1:]
+        #    
+        #    # get the underlying source table
+        #    src_tab = f_cols.get((col, tab))
+
         res[col] = {
-            'tab': tab,
-            'expr': expr
+            #'tab': tab,
+            'expr': expr,
+            #'src_tab': src_tab
         }
 
-    return res
+    # return results as data frame
+    df = pd.DataFrame.from_dict(res, orient='index')
+    df.index.name = 'columns'
+    return df
